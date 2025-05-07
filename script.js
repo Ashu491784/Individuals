6dfb4431cd3d0761ba00df8f1cd7172169b53a6a
Admin <ayeshamadushani@icloud.com>
2025-05-07 13:06:39 -0700

    ui updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const API_CONFIG ={
-    APP_KEY: '4eb1f545d6fa4ef086d3e78db0afb0fd',
+    APP_KEY: '9bbb51b95dbc9d2e17f253f3d74b05a6',
+    BASE_URL: 'https://gnews.io/api/v4/top-headlines?token=9bbb51b95dbc9d2e17f253f3d74b05a6&lang=en&country=lk'
 
 }
 
@@ -15,4 +16,49 @@ const saveNewsBtn = document.getElementById('save-new-btn');
 const fovouriteModal = new bootstrap.Modal(document.getElementById('favouritesModal'));
 const favouritesModelBody = document.getElementById('favouritemodalBody');
 const nofavouriteMessage = document.getElementById('nofavouriteMessage');
+
+let currentnews = [];
+let currentnewDetails = null;
+let favourites = JSON.parse(localStorage.getItem('newsfavourites')) || [];
+
+function init(){
+    searchbtn.addEventListener('click', handleSearch);
+    searchInput.addEventListener('keypress', function(e){
+        if(e.key == 'Enter') handleSearch();
+    });
+
+    quicksearchBtn.forEach(btn =>{
+        btn.addEventListener('click', function(){
+            searchInput.value = this.dataset.query;
+            handleSearch();
+        })
+    });
+    showFavouriteBtn.addEventListener('click', showFavourites);
+     saveNewsBtn.addEventListener('click', togglefavourites);  
+     
+     renderFavourites();
+    
+}
+
+function handleSearch(){
+    const query = searchInput.value.trim();
+    if(query){
+        fetchNews(query);
+    }else{
+        alert('please enter a news paper name');
+    }
+}
+
+async function fetchNews(query){
+    try{
+
+        showloadingstatus();
+
+        const url = new URL(API_CONFIG.BASE)
+
+    }catch(error){
+        showErrorMessage(error);
+
+    }
+}
 
