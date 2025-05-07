fb0626f300cb32922251c69f177f5b0b04f3b472
Admin <ayeshamadushani@icloud.com>
2025-05-07 11:34:08 -0700

    ui updated

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const API_CONFIG{
-    APP_KEY
+const API_CONFIG ={
+    APP_KEY: '4eb1f545d6fa4ef086d3e78db0afb0fd',
+
 }
+
+const searchInput = document.getElementById('search-input');
+const searchbtn = document.getElementById('search-btn');
+const newsConteiner = document.getElementById('news-conteiner');
+const quicksearchBtn = document.querySelectorAll('quick-search');
+const showFavouriteBtn = document.getElementById('show-favourites');
+const newsmodel = new bootstrap.Modal(document.getElementById('newsModel'));
+const newsmodelTitle = document.getElementById('newsModelTitle');
+const newsModalBody = document.getElementById('newsModalBody');
+const saveNewsBtn = document.getElementById('save-new-btn');
+const fovouriteModal = new bootstrap.Modal(document.getElementById('favouritesModal'));
+const favouritesModelBody = document.getElementById('favouritemodalBody');
+const nofavouriteMessage = document.getElementById('nofavouriteMessage');
+
